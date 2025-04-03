446189cd955c96b13140720b913aa3015ae86aa5
Laith Taher <laith@claim.co>
2025-04-02 23:58:59 -0400

    removed comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -566,20 +566,4 @@ require("lazy").setup({
 			{ "<leader>gg", "<cmd>LazyGit<cr>" },
 		},
 	},
-
-	-- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-	-- init.lua. If you want these files, they are in the repository, so you can just download them and
-	-- place them in the correct locations.
-
-	-- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-	--
-	--  Here are some example plugins that I've included in the Kickstart repository.
-	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
-	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 })
