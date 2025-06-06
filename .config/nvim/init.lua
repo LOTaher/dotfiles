705592f972fe208d041512719307778e04dc606f
Laith Taher <laith@claim.co>
2025-06-06 09:53:38 -0400

    fixed lsp restart errors

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -297,7 +297,7 @@ require("lazy").setup({
 						group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
 						callback = function(event2)
 							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
+							-- vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
 						end,
 					})
 				end,
